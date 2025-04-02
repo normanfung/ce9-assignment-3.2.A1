1a0d346ec915cd55297a90c364836670ab620813
normanfung <56857601+normanfung@users.noreply.github.com>
2025-04-02 20:21:02 +0800

    Hotfix 2

--- backend.tf
+++ backend.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "norman-ce9-module3-lesson2-A1.tfstate" # Replace the value of key to <your suggested name>.tfstat   
+    key    = "module3/lesson2/norman-a1.tfstate" # Suggested naming convention
     region = "us-east-1"
   }
 
@@ -22,10 +22,15 @@ terraform {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+  bucket = local.name_prefix + "-s3-tf-bkt-" + local.account_id
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "dev"
+  }
 }
