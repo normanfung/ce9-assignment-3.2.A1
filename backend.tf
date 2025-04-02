ad397a1b874efbcf942a35836d8889e79114fcf7
normanfung <56857601+normanfung@users.noreply.github.com>
2025-04-02 20:17:41 +0800

    Hotfix 1

--- backend.tf
+++ backend.tf
@@ -8,6 +8,15 @@ terraform {
     key    = "norman-ce9-module3-lesson2-A1.tfstate" # Replace the value of key to <your suggested name>.tfstat   
     region = "us-east-1"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.4.0"
 }
 
 data "aws_caller_identity" "current" {}
