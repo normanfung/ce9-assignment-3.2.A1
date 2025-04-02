82f3755c0e4bf6b388f201e3a9c3fad50b71afb3
normanfung <56857601+normanfung@users.noreply.github.com>
2025-04-02 20:25:13 +0800

    Hotfix 4

--- backend.tf
+++ backend.tf
@@ -27,7 +27,7 @@ locals {
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket = local.name_prefix + "-s3-tf-bkt-" + tostring(local.account_id)
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 
   tags = {
     Name        = "Terraform State Bucket"
